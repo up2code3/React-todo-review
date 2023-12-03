cd01af5e96161a7e051c3d942d26f93e617341f7
Tristan White <113216091+up2code3@users.noreply.github.com>
2023-12-03 12:05:57 -0500

    create array use dot map to make it a li

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
+
+const todoList = [
+    { id: 0 , title: "eat"},
+    { id: 1 , title: "sleep"},
+    { id: 2 , title: "run"},
+];
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+   <div>
+    <h1>TODO LIST</h1>
+    <ul>
+    {todoList.map((todoList) => (
+     <li key={todoList.id}>{todoList.title} </li>   
+    ))}
+    </ul>
+   </div> 
+  )
 }
 
 export default App;
