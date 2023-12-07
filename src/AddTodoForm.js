28f37f1c95098a32edf2da7cb9693f1087913f81
Tristan White <113216091+up2code3@users.noreply.github.com>
2023-12-07 08:24:23 -0500

    lesson 1.1 and 1.2 complete

---
+++ src/AddTodoForm.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+const AddTodoForm = () => {
+  return (
+    <form>
+      <label htmlFor="todoTitle">"Title"</label>
+      <input id="todoTitle"></input>
+      <button>"add"</button>
+    </form>
+  )
+};
+
+export default AddTodoForm;
